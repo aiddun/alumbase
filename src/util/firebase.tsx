d95616106716e35d9737a616f9a8e533e3030fe9
Aidan Dunlap <aidan@cs.utexas.edu>
2021-06-13 18:17:04 -0400

    i'm an idiot

--- src/util/firebase.tsx
+++ src/util/firebase.tsx
@@ -5,16 +5,6 @@ import "firebase/auth";
 import "firebase/storage";
 import "firebase/functions";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDy1MGvxdINrkDO-GIolbqnFbqUmWISD98",
-  authDomain: "alumbase-adts.firebaseapp.com",
-  projectId: "alumbase-adts",
-  storageBucket: "alumbase-adts.appspot.com",
-  messagingSenderId: "601692437336",
-  appId: "1:601692437336:web:fbf4ed72854339f6a7432d",
-};
-
-console.log(import.meta.env)
 firebase.initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG));
 
 export const functions = firebase.functions();
